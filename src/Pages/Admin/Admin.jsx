c2fb20b78db706bb114bf3be2704f9ba92dc7016
Xuan Son <doxuanson12b2003@gmail.com>
2024-05-11 12:06:37 +0700

    feat: Add Navbar, Sidebar, Admin components

---
+++ src/Pages/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import './Admin.scss';
+
+const Admin = () => {
+    return (
+        <div className="admin">
+            <Sidebar />
+        </div>
+    );
+};
+
+export default Admin;
